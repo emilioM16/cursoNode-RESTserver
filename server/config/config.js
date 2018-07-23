3960322544bd1ed757108307cfaaacb57415aba1
Emilio Melo <emiliops11@gmail.com>
2018-07-23 01:28:01 -0300

    RestSever - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,24 @@
 // PUERTO  
 // ================================
 process.env.PORT = process.env.PORT || 3000;
+
+// ================================
+// ENTORNO  
+// ================================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ================================
+// BASE DE DATOS  
+// ================================
+
+let urlDB;
+
+if(process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else{
+    urlDB = 'mongodb://cafe_user:cafenode18@ds147461.mlab.com:47461/cafe_node';
+}
+
+process.env.URLDB = urlDB;
+
--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 require('./config/config');
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const options = {
+    useNewUrlParser:true
+}
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -9,29 +15,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(require('./routes/usuario'));
 
-app.get('/usuario', function (req, res) {
-  res.json('Hello World')
-});
- 
-app.post('/usuario', function (req, res) {
-    let body = req.body;
-    res.json({
-        body
-    })
-});
+mongoose.connect(process.env.URLDB, options, (err, res) => {
+    if(err) throw err;
+    console.log('Conexión a la base de datos realizada');
 
-app.put('/usuario/:id', function (req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    });
 });
-
-app.delete('/usuario', function (req, res) {
-    res.json('Hello World')
-});
-
 
 app.listen(process.env.PORT, ()=> {
     console.log('Escuchando el puerto', process.env.PORT);
